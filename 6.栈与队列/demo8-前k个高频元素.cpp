82cd328a9bab4f59263e9f79308ff1e68ed27022
yuanjl <1811905705@qq.com>
2024-10-06 19:27:18 +0800

    ignore

--- 6.栈与队列/demo8-前k个高频元素.cpp
+++ 6.栈与队列/demo8-前k个高频元素.cpp
@@ -3,13 +3,20 @@
 #include <queue>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+	{
+		unordered_map<int, int> umap;
+		for(int i = 0; i < nums.size();i++)
+		{
+			umap[nums[i]]++;
+		}
+		cout << umap[3];
     }
 };
 
@@ -17,10 +24,10 @@ public:
 int main()
 {
 	Solution* s = new Solution();
-	vector<int> nums = {1,3,-1,-3,5,3,6,7};
+	vector<int> nums = {1,3,5,3,6,3,7};
 	vector<int> ret = s->topKFrequent(nums,3);
-	for(int i = 0; i< ret.size();i++)
-		cout << ret[i] << endl;
+//	for(int i = 0; i< ret.size();i++)
+//		cout << ret[i] << endl;
     return 0;
 }
 
