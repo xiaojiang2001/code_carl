4b99eadd7ebc65363d67d2b2ce69f68d6f3cd0dc
yuanjl <1811905705@qq.com>
2024-11-29 02:56:34 +0000

    greedy

--- greedy/demo14根据身高重建队列.cpp
+++ greedy/demo14根据身高重建队列.cpp
@@ -4,12 +4,28 @@
 
 using namespace  std;  
 
+// 先根据人数排序
+static bool cmp(vector<int>& left, vector<int>& right)
+{
+    if(left[0] != right[0])
+        return left[0] > right[0];
+    else
+        return left[1] < right[1];
+}
+
 vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        
+    // 身高从大到小排，确定了每个人前面身高更高
+    sort(people.begin(), people.end(), cmp);
+    vector<vector<int>> res;
+    // 根据k插入到队列 因为前面的都比他高 那么就是根据k依次确定下标
+    for (auto it : people) {
+        res.insert(res.begin() + it[1], it);
+    }
+    return res;
 }
 
 int main()
 {
     vector<vector<int>> people{{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
-    reconstructQueue(people);
+    reconstructQueue(people);    
 }
--- greedy/demo3摆动序列.cpp
+++ greedy/demo3摆动序列.cpp
@@ -5,32 +5,24 @@
 using namespace std;
     
 int wiggleMaxLength(vector<int>& nums) {
-    // 插入个0, 计算第一个差值
-    nums.insert(nums.begin(), 0);
-    unordered_set<int> uset;
-    for (int i = 0; i < nums.size(); i++)
-        uset.insert(nums[i]);
-
-    int res = 1;    
-    // 初始化第一个差值性
-    if (uset.size() <= 1)
-        return uset.size();
-    auto it = uset.begin();
-
+    // int prediff = nums[i] - nums[i-1];
+    // int curdiff = nums[i+1] - nums[i];
+   
+    if(nums.size() == 1)
+        return 1;
+    int result = 1;     // 默认数组尾有一个摆动
+    int prediff = 0;    // 类似添加了一个虚拟头节点 平坡 
+    int curdiff;        // 初始化任意值均可
     for (int i = 0; i < nums.size() - 1; i++)
     {
-        // 计算当前差值性
-        bool temp = nums[i+1] - nums[i] > 0 ? true : false;
-
-        // 判断差值性是否一致
-        if (temp == flag && nums[i+1] != nums[i]) {
-            flag = !flag;
-            res++;
-        }    
+        curdiff = nums[i+1] - nums[i]; 
+        if(prediff <= 0 && curdiff > 0 || prediff >= 0 && curdiff < 0) {
+            result++;
+            prediff = curdiff;
+        }
     }
-    return res;
+    return result;
 }
-
 int main()
 {
     vector<int> nums{3,3,3,2,5};
